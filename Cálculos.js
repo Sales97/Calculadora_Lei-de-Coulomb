273dc07f86bc8025e410d002e552c5fedf8d6324
Imperadorvader97 <65308207+Imperadorvader97@users.noreply.github.com>
2021-09-16 16:07:52 -0300

    Update Cálculos.js

--- Cálculos.js
+++ Cálculos.js
@@ -3,13 +3,13 @@ function calcular (){
     var a2 = document.getElementById("carga2")
     var a3 = document.getElementById("distancia")
 
-    var q1 = Number(a1.value)
-    var q2 = Number(a2.value)
+    var q1 = Number(a1.value)/1000000
+    var q2 = Number(a2.value)/1000000
     var d = Number(a3.value)
     const K = 9000000000
 
     var distancia = d*d
-    var x = K*q1*q2
+    var x = K*q1*q2*100
     var y = x/distancia
     var forca = Math.abs(y)
 
